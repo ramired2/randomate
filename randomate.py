e0abe11f0cb9b859561cf2373d88c97e8b8e4850
Daisy <ramired2@oregonstate.edu>
2021-08-11 12:12:04 -0700

    added the n amnt ofitems in list functionality

--- randomate.py
+++ randomate.py
@@ -11,50 +11,96 @@ def homepage():
 
     with open("countriesSpotify.json", 'r') as l:
             countries = json.loads(l.read())
+    country = 'US'
     
     # when entering diff country
     # json file that has countries n abbrevs needed
     p = request.form.get('p')
-    if p:
+    n = request.form.get('n')
+    if p and n:
+        n=int(n)
+        country = p
+        dataCharts = read(p.strip(), n)
+    elif p:
+        country = p
         dataCharts = read(p.strip(), 10)
     else:
         dataCharts = read('us', 10)
 
     # when entering playlist search
     q = request.args.get("q")
-    if q:
+    size = request.args.get("size")
+    # print(size)
+    if q and size:
+        size=int(size)
+        print(size)
+        processSearch(q, size)
+        
+        with open("dataSearch.json", 'r') as j:
+            dataPlaylists = json.loads(j.read())
+        return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
+    elif q:
+        print("just q")
         processSearch(q, 5)
         
         with open("dataSearch.json", 'r') as j:
             dataPlaylists = json.loads(j.read())
         return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
 
-    return render_template("homepage.html", title="Home", dataCharts=dataCharts, countries=countries)
+    return render_template("homepage.html", title="Home", dataCharts=dataCharts, countries=countries, selected=country.upper())
 
-# specific page for getting playlists
-@app.route("/search/<query>", methods=["GET", "POST"])
-def search(query):
+# specific page for getting playlists, must input number if using this link
+@app.route("/search/<query>/<num>", methods=["GET", "POST"])
+def search(query, num):
 
     q = request.args.get("q")
-    if q:
-        processSearch(q, 5)
+    size = request.args.get("size")
+
+    if q and size:
+        size=int(size)
+        processSearch(q, size)
+        
         with open("dataSearch.json", 'r') as j:
             dataPlaylists = json.loads(j.read())
         return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
-
-    processSearch(query, 5)
-    with open("dataSearch.json", 'r') as j:
+    elif q:
+        processSearch(q, 5)
+        
+        with open("dataSearch.json", 'r') as j:
             dataPlaylists = json.loads(j.read())
-    return render_template("search.html", title="Search "+query, search=query, dataPlaylists=dataPlaylists)
+        return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
+    elif query and num:
+        num=int(num)
+        processSearch(query, num)
+        with open("dataSearch.json", 'r') as j:
+                dataPlaylists = json.loads(j.read())
+        return render_template("search.html", title="Search "+query.upper(), search=query, dataPlaylists=dataPlaylists)
+    elif query:
+        processSearch(query, 5)
+        with open("dataSearch.json", 'r') as j:
+                dataPlaylists = json.loads(j.read())
+        return render_template("search.html", title="Search "+query.upper(), search=query, dataPlaylists=dataPlaylists)
+
 
 # FOR ERRORS
 
 @app.errorhandler(404)
 def not_found(error):
     q = request.args.get("q")
-    if q:
+    size = request.args.get("size")
+
+    if q and size:
+        size=int(size)
+        print(size)
+        processSearch(q, size)
+        
+        with open("dataSearch.json", 'r') as j:
+            dataPlaylists = json.loads(j.read())
+        return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
+    elif q:
+        # print("just q")
         processSearch(q, 5)
-        # dataPlaylists = json.load(g)
+        
         with open("dataSearch.json", 'r') as j:
             dataPlaylists = json.loads(j.read())
         return render_template("search.html", title="Search "+q, search=q, dataPlaylists=dataPlaylists)
